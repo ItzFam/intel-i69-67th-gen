86f093c2bb64eafd2ef37bdd123764ada68152aa
Fahim Abdullah Mahdi <famahdi13@gmail.com>
2026-08-03 22:19:27 +0400

    Base model of my intel i69 cpu. (Intel pls hire me)

---
+++ CPU.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+
+class CPU
+{
+public:
+    // Uh dont know what to call it but I guess the boxes that you saw in LMC.
+    int registers[4], memory[256] = {0}, programCounter = 0, instruction, accumulator = 0;
+
+    bool running = true;
+
+    std::string operation;
+
+    // Useful Functions (No I am not going to explain them here)
+    void run(), fetch(), decode(), execute();
+};
+
+void CPU::fetch() // Retrieves the instruction in the memory.
+{
+    instruction = memory[programCounter];
+}
+
+void CPU::decode() // Decodes instruction.
+{
+
+    if (instruction == 0)
+    {
+        operation = "HALT";
+    }
+    else if (instruction == 901)
+    {
+        operation = "INPUT";
+    }
+    else if (instruction == 902)
+    {
+        operation = "OUTPUT";
+    }
+    else if (instruction / 100 == 1)
+    {
+        operation = "ADD";
+    }
+}
+
+void CPU::execute() // Executes the instruction.
+{
+    int memory_address, data;
+
+    if (operation == "HALT")
+    {
+        running = false;
+    }
+    else if (operation == "INPUT")
+    {
+        std::cout << "Input: ";
+        std::cin >> accumulator;
+
+        programCounter++;
+    }
+    else if (operation == "OUTPUT")
+    {
+        std::cout << "Output: \n"
+                  << accumulator;
+
+        programCounter++;
+    }
+    else if (operation == "ADD")
+    {
+        memory_address = instruction % 100;
+        data = memory[memory_address];
+
+        accumulator += data;
+
+        programCounter++;
+    }
+}
+
+void CPU::run()
+{
+    while (running)
+    {
+        fetch();
+        decode();
+        execute();
+    }
+}
+
+int main()
+{
+    CPU cpu;
+
+    cpu.memory[0] = 901;
+    cpu.memory[1] = 113;
+    cpu.memory[2] = 902;
+    cpu.memory[13] = 46;
+
+    cpu.run();
+
+    return 0;
+}
